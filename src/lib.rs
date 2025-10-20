7bb14bd83ca2acea1be4a6838d6f654ebfe427cf
impliedfeline <kinnunen.jussi@hotmail.com>
2025-10-20 23:38:08 +0300

    refactor: separate ProgramState into separate struct

--- src/lib.rs
+++ src/lib.rs
@@ -54,7 +54,7 @@ impl FromStr for Program {
                     // TODO: Error handling for unbalanced parens
                     let jump_addr = jump_stack.pop().unwrap();
                     if let Instruction::JumpLeft(_) = prog[jump_addr] {
-                        prog[jump_addr] = Instruction::JumpLeft(i)
+                        prog[jump_addr] = Instruction::JumpLeft(i);
                     }
                     Instruction::JumpRight(jump_addr)
                 }
@@ -66,52 +66,63 @@ impl FromStr for Program {
     }
 }
 
-impl Program {
-    pub fn run<T: Read, U: Write, const LEN: usize>(
-        &self,
-        data: &mut [u8; LEN],
-        mut data_ptr: usize,
-        mut input: T,
-        output: &mut U,
-    ) -> usize {
-        let program = &self.0;
-        let mut instr_ptr: usize = 0;
-        loop {
-            let instr = {
-                if program.len() <= instr_ptr {
-                    break;
-                } else {
-                    &program[instr_ptr]
-                }
-            };
-            match instr {
-                Instruction::MoveLeft => data_ptr -= 1,
-                Instruction::MoveRight => data_ptr += 1,
-                Instruction::Increment => data[data_ptr] += 1,
-                Instruction::Decrement => data[data_ptr] -= 1,
-                Instruction::Write => {
-                    output.write_all(&[data[data_ptr]]).unwrap();
-                }
-                Instruction::Read => {
-                    let mut buffer = [0];
-                    input.read_exact(&mut buffer).unwrap();
-                    data[data_ptr] = buffer[0];
-                }
-                Instruction::JumpLeft(jump_addr) => {
-                    if data[data_ptr] == 0 {
-                        instr_ptr = *jump_addr;
-                        continue;
-                    }
-                }
-                Instruction::JumpRight(jump_addr) => {
-                    if data[data_ptr] != 0 {
-                        instr_ptr = *jump_addr;
-                        continue;
-                    }
+#[derive(Debug)]
+pub struct ProgramState<const LEN: usize> {
+    pub data: [u8; LEN],
+    pub data_ptr: usize,
+    pub instr_ptr: usize,
+}
+
+impl<const LEN: usize> Default for ProgramState<LEN> {
+    fn default() -> Self {
+        Self {
+            data: [0u8; LEN],
+            data_ptr: Default::default(),
+            instr_ptr: Default::default(),
+        }
+    }
+}
+
+impl<const LEN: usize> ProgramState<LEN> {
+    pub fn run<T: Read, U: Write>(&mut self, prog: &Program, input: &mut T, output: &mut U) {
+        while let Some(instr) = prog.0.get(self.instr_ptr) {
+            self.step(*instr, input, output);
+        }
+    }
+
+    /// Runs a single instruction against a given `ProgramState`.
+    ///
+    /// # Panics
+    ///
+    /// Currently, the program panics in various different illegal states for a program state.
+    /// In a later commit this function will be made panic-free, but that is still to do.
+    pub fn step<T: Read, U: Write>(&mut self, instr: Instruction, input: &mut T, output: &mut U) {
+        match instr {
+            Instruction::MoveLeft => self.data_ptr -= 1,
+            Instruction::MoveRight => self.data_ptr += 1,
+            Instruction::Increment => self.data[self.data_ptr] += 1,
+            Instruction::Decrement => self.data[self.data_ptr] -= 1,
+            Instruction::Write => {
+                output.write_all(&[self.data[self.data_ptr]]).unwrap();
+            }
+            Instruction::Read => {
+                let mut buffer = [0];
+                input.read_exact(&mut buffer).unwrap();
+                self.data[self.data_ptr] = buffer[0];
+            }
+            Instruction::JumpLeft(jump_addr) => {
+                if self.data[self.data_ptr] == 0 {
+                    self.instr_ptr = jump_addr;
+                    return;
                 }
             }
-            instr_ptr += 1;
+            Instruction::JumpRight(jump_addr) => {
+                if self.data[self.data_ptr] != 0 {
+                    self.instr_ptr = jump_addr;
+                    return;
+                }
+            }
         }
-        data_ptr
+        self.instr_ptr += 1;
     }
 }
