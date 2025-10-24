76492cb4076707ec9d56593076ca595b4914b215
impliedfeline <kinnunen.jussi@hotmail.com>
2025-10-24 18:22:52 +0300

    refactor: move run and step to `Instruction` and `Program`

--- src/program.rs
+++ src/program.rs
@@ -1,6 +1,11 @@
-use std::str::FromStr;
+use std::{
+    io::{Read, Write},
+    str::FromStr,
+};
 
 use thiserror::Error;
+
+use crate::state::ProgramState;
 
 #[derive(Copy, Clone, Debug)]
 pub enum Instruction {
@@ -15,9 +20,48 @@ pub enum Instruction {
 }
 
 impl Instruction {
-    fn is_token(c: char) -> bool {
+    pub fn is_token(c: char) -> bool {
         let token_chars = ['<', '>', '+', '-', '.', ',', '[', ']'];
         token_chars.contains(&c)
+    }
+
+    /// Runs a single instruction against a given `ProgramState`.
+    ///
+    /// # Panics
+    ///
+    /// Currently, the program panics in various different illegal states for a program state.
+    /// In a later commit this function will be made panic-free, but that is still to do.
+    pub fn step<I: Read, O: Write, const LEN: usize>(
+        self,
+        state: &mut ProgramState<LEN>,
+        input: &mut I,
+        output: &mut O,
+    ) -> Option<usize> {
+        match self {
+            Instruction::MoveLeft => state.data_ptr -= 1,
+            Instruction::MoveRight => state.data_ptr += 1,
+            Instruction::Increment => state.data[state.data_ptr] += 1,
+            Instruction::Decrement => state.data[state.data_ptr] -= 1,
+            Instruction::Write => {
+                output.write_all(&[state.data[state.data_ptr]]).unwrap();
+            }
+            Instruction::Read => {
+                let mut buffer = [0];
+                input.read_exact(&mut buffer).unwrap();
+                state.data[state.data_ptr] = buffer[0];
+            }
+            Instruction::JumpLeft(jump_addr) => {
+                if state.data[state.data_ptr] == 0 {
+                    return Some(jump_addr);
+                }
+            }
+            Instruction::JumpRight(jump_addr) => {
+                if state.data[state.data_ptr] != 0 {
+                    return Some(jump_addr);
+                }
+            }
+        }
+        None
     }
 }
 
@@ -29,6 +73,22 @@ pub enum ParseError {
 
 #[derive(Debug)]
 pub struct Program(pub Vec<Instruction>);
+
+impl Program {
+    pub fn run<I: Read, O: Write, const LEN: usize>(
+        &self,
+        mut instr_ptr: usize,
+        state: &mut ProgramState<LEN>,
+        input: &mut I,
+        output: &mut O,
+    ) {
+        while let Some(instr) = self.0.get(instr_ptr) {
+            if let Some(jump_addr) = instr.step(state, input, output) {
+                instr_ptr = jump_addr;
+            }
+        }
+    }
+}
 
 impl FromStr for Program {
     type Err = ParseError;
